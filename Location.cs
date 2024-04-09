5663c7d8ca8ddc283ec71b8857e97dd4967c9243
AlexaderPeev <163998898+AlexaderPeev@users.noreply.github.com>
2024-04-09 11:54:34 +0300

    Add files via upload

---
+++ Location.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
