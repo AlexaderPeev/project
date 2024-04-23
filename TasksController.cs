66cbc37071efc97697fbe6d1873291ddb5ea2a0e
AlexaderPeev <163998898+AlexaderPeev@users.noreply.github.com>
2024-04-23 11:06:56 +0300

    Add files via upload

---
+++ TasksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly YourDbContext _context;
+
+        public TasksController(YourDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
